11d79ce8102b005b77a7285ec72dfb84c82f8841
JakubLukaszuk <41912745+JakubLukaszuk@users.noreply.github.com>
2020-05-01 16:19:37 +0200

    First commit
    Walking skeleton API.

---
+++ API/Program.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var sevices = scope.ServiceProvider;
+                try
+                {
+                    var context = sevices.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                }
+                catch(Exception ex)
+                {
+                    var logger = sevices.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex,"msg");
+                }
+            }
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
